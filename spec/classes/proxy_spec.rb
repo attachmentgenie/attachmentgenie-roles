1934195ca47158e087a230540f0e29e6dcbfdbd4
Bram Vogelaar <attachmentgenie@inuits.eu>
2016-05-07 11:17:26 +0200

    supplying all facts required by all fixtures

--- spec/classes/proxy_spec.rb
+++ spec/classes/proxy_spec.rb
@@ -1,8 +1,13 @@
 require 'spec_helper'
 describe 'roles::proxy' do
-
+  let :facts do
+  {
+    :osfamily       => 'RedHat',
+    :ipaddress      => '127.0.0.1',
+    :ipaddress_eth1 => '127.0.0.1',
+  }
+  end
   context 'with defaults for all parameters' do
-    let(:facts) { {:osfamily => 'Debian'} }
     it { should contain_class('roles::proxy') }
   end
 end
