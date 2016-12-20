2b23bac7aadde448e0f593389ed8852b38365cbd
Bram Vogelaar <attachmentgenie@inuits.eu>
2016-12-20 10:36:54 +0100

    merging neo4j stack into database stack, adding mongodb to database stack

---
+++ manifests/database.pp
@@ -0,0 +1,13 @@
+# This class can be used to setup a database stack.
+#
+# @example when declaring the database role
+#  class { '::roles::database': }
+#
+class roles::database () {
+  class { '::stacks::bootstrap': } ->
+  class { '::stacks::monitoring': } ->
+  class { '::stacks::orchestration': } ->
+  class { '::stacks::runtime': } ->
+  class { '::stacks::security': } ->
+  class { '::stacks::database': }
+}
--- spec/classes/neo4j_spec.rb
+++
@@ -1,6 +0,0 @@
-require 'spec_helper'
-describe 'roles::neo4j' do
-  context 'with defaults for all parameters' do
-    it { should contain_class('roles::neo4j') }
-  end
-end
