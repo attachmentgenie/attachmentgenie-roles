e234a158691ee78f107126ee1059ceec54dc7074
Bram Vogelaar <bram@inuits.eu>
2017-01-26 11:06:26 +0100

    splitting puppetmaster stack in cache, mq and puppet

--- manifests/puppetmaster.pp
+++ manifests/puppetmaster.pp
@@ -5,15 +5,21 @@
 #
 class roles::puppetmaster () {
   class { '::stacks::bootstrap': } ->
+  class { '::stacks::cache': } ->
   class { '::stacks::database': } ->
   class { '::stacks::monitoring': } ->
+  class { '::stacks::mq': } ->
   class { '::stacks::orchestration': } ->
   class { '::stacks::runtime': } ->
   class { '::stacks::security': } ->
-  class { '::stacks::puppetmaster': }
+  class { '::stacks::puppet': }
 
   if defined(Class['profiles::postgresql']) and defined(Class['profiles::puppetdb']) {
     Postgresql::Server::Db <||> -> Class['::puppetdb::server']
     Postgresql::Server::Db <||> -> Class['::puppetdb::server::validate_db']
+  }
+
+  if defined(Class['profiles::memcached']) and defined(Class['profiles::foreman']) {
+    Class['::memcached'] -> Class['::foreman']
   }
 }
