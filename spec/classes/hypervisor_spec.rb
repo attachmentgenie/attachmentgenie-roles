d045755b7777a859f90cae0ae8cd89bb69689f00
Bram Vogelaar <attachmentgenie@inuits.eu>
2018-10-17 16:25:05 +0200

    fixing style problems

--- spec/classes/hypervisor_spec.rb
+++ spec/classes/hypervisor_spec.rb
@@ -3,7 +3,7 @@ describe 'roles::hypervisor' do
   on_supported_os.each do |os, facts|
     context "on #{os}" do
       let(:facts) { facts }
-      
+
       context 'with defaults for all parameters' do
         it { is_expected.to contain_class('roles::hypervisor') }
         it { is_expected.to contain_class('roles::node') }
