54ecbbfd86d03f894d122aa30911ad30b293b8a0
Bram Vogelaar <attachmentgenie@inuits.eu>
2019-05-19 21:27:24 +0200

    bumping to latest PDK, renaming hypervisor to scheduler since that covers the actual content better

---
+++ manifests/scheduler.pp
@@ -0,0 +1,10 @@
+# This class can be used to setup a scheduler stack.
+#
+# @example when declaring the scheduler role
+#  class { '::roles::scheduler': }
+#
+class roles::scheduler inherits roles::node {
+  anchor { 'scheduler::begin': }
+  -> class { '::profiles::scheduling': }
+  -> anchor { 'scheduler::end': }
+}
--- spec/classes/scheduler_spec.rb
+++ spec/classes/scheduler_spec.rb
@@ -1,14 +1,14 @@
 require 'spec_helper'
-describe 'roles::hypervisor' do
+describe 'roles::scheduler' do
   on_supported_os.each do |os, facts|
     context "on #{os}" do
       let(:facts) { facts }
 
       context 'with defaults for all parameters' do
-        it { is_expected.to contain_class('roles::hypervisor') }
+        it { is_expected.to contain_class('roles::scheduler') }
         it { is_expected.to contain_class('roles::node') }
-        it { is_expected.to contain_anchor('hypervisor::begin') }
-        it { is_expected.to contain_anchor('hypervisor::end') }
+        it { is_expected.to contain_anchor('scheduler::begin') }
+        it { is_expected.to contain_anchor('scheduler::end') }
       end
     end
   end
