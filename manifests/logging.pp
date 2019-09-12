4eb95f3a6ae3fdd2f7128cdc53ff044a59ffb9ac
Bram Vogelaar <attachmentgenie@inuits.eu>
2019-09-12 22:44:57 +0200

    no longer need to make sure we have order

--- manifests/logging.pp
+++ manifests/logging.pp
@@ -8,13 +8,4 @@ class roles::logging inherits roles::node {
   -> class { '::profiles::logging': }
   -> class { '::profiles::website': }
   -> anchor { 'logging::end': }
-
-  if $::osfamily == 'RedHat' {
-    if defined(Class['profiles::runtime::java']) and defined(Class['profiles::monitoring::logstash']) {
-      Package['java'] -> Yumrepo['elastic-5.x']
-    }
-    if defined(Class['profiles::runtime::java']) and defined(Class['profiles::logging::elasticsearch']) {
-      Package['java'] -> Yumrepo['elasticsearch']
-    }
-  }
 }
