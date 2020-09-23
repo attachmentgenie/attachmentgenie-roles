2f128ac1aa53692124b8a1c82b7285c69a43da91
Bram Vogelaar <bram@attachmentgenie.com>
2020-09-23 23:58:25 +0200

    initial import of minio s3 storage

---
+++ manifests/storage.pp
@@ -0,0 +1,10 @@
+# This class can be used to setup a storage stack.
+#
+# @example when declaring the storage role
+#  class { '::roles::storage': }
+#
+class roles::storage inherits roles::node {
+  anchor { 'storage::begin': }
+  -> class { '::profiles::storage': }
+  -> anchor { 'storage::end': }
+}
