5acf6dd48c5baff6552ebd9c66186c809441756c
Bram Vogelaar <bram@attachmentgenie.com>
2021-04-16 00:55:57 +0200

    moving mail into node level

--- manifests/metrics.pp
+++ manifests/metrics.pp
@@ -6,7 +6,6 @@
 class roles::metrics inherits roles::node {
   anchor { 'metrics::begin': }
   -> class { '::profiles::database': }
-  -> class { '::profiles::mail': }
   -> class { '::profiles::alerting': }
   -> class { '::profiles::website': }
   -> anchor { 'metrics::end': }
