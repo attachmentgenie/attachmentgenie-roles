c03757fc8d745b739c4970b65c1b49cb95a14772
Bram Vogelaar <bram@attachmentgenie.com>
2021-10-14 00:30:08 +0200

    removing unused bits, now we removed activemq

--- manifests/puppetmaster.pp
+++ manifests/puppetmaster.pp
@@ -6,17 +6,13 @@
 class roles::puppetmaster inherits roles::node {
   anchor { 'puppetmaster::begin': }
   -> class { '::profiles::cache': }
+  -> class { '::profiles::testing': }
   -> class { '::profiles::database': }
-  -> class { '::profiles::mq': }
   -> class { '::profiles::puppet': }
   -> anchor { 'puppetmaster::end': }
 
   if defined(Class['profiles::database::postgresql']) and defined(Class['profiles::puppet::puppetdb']) {
     Postgresql::Server::Db <||> -> Class['::puppetdb::server']
     Postgresql::Server::Db <||> -> Class['::puppetdb::server::validate_db']
-  }
-
-  if defined(Class['profiles::cache::memcached']) and defined(Class['profiles::puppet::foreman']) {
-    Class['::memcached'] -> Class['::foreman']
   }
 }
